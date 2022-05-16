b96a14e77b629161b9e59fc64ba240d781e5e2f9
Asim Ali <81342795+asim-ali5514@users.noreply.github.com>
2022-05-15 17:43:44 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/globals.css";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-red-700">hi</h1>
+    </>
+  );
+};
+
+export default Home;
